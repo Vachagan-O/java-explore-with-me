0a7ae2616b7116f444afbf280c5e0eba56f936c5
Vachagan <va4agan.oganesian@yandex.ru>
2023-08-15 11:30:49 +0300

    Дипломная v3.0

--- main-service/src/main/java/ru/practicum/main_service/comment/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/main_service/comment/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package ru.practicum.main_service.comment.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.main_service.comment.model.Comment;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByAuthorId(Long userId);
 
